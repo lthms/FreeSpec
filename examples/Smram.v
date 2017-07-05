6bc1d83f16e7ff8c16bf2e32dcc8016dc9d2da14
Thomas Letan <thomas.letan@ssi.gouv.fr>
2017-07-05 14:33:55 +0200

    First definition of the Smram Contract

--- examples/Smram.v
+++ examples/Smram.v
@@ -1,11 +1,14 @@
 (* begin hide *)
 Require Import Coq.Bool.Sumbool.
+Require Import Coq.Program.Equality.
+
 (* end hide *)
 
 Require Import FreeSpec.Interp.
 Require Import FreeSpec.Compose.
 Require Import FreeSpec.Refine.
 Require Import FreeSpec.Program.
+Require Import FreeSpec.Contract.
 
 Require Import FreeSpec.Examples.Map.
 
@@ -29,6 +32,7 @@ Section SMRAM_EXAMPLE.
       into the SMRAM.
     *)
   Variables (Addr: Type)
+            (addr_eq: forall (a a': Addr), {a = a'}+{a <> a'})
             (Value: Type)
             (Smram: Addr -> Prop)
             (Smramdec: forall (a: Addr), {Smram a}+{~Smram a}).
@@ -148,4 +152,99 @@ Section SMRAM_EXAMPLE.
             ret (tt, s)
       (* ---------------------------------------------------------- *)
       end.
+
+  (** ** Contract
+
+      Now here comes the section where FreeSpec is supposed to shine.
+      We want to define a Contract which basically says: “Privileged
+      read accesses only fetch value written by privileged write
+      accesses.”
+
+      *** Abstract State
+
+      To express that, we first define a very simple abstract state
+      which is basically a map from SMRAM addresses to value written
+      by privileged write accesses.
+
+      To do that, we will keep a map from SMRAM addresses to value
+      which will be updated only with privileged memory accesses.
+
+   *)
+
+  Definition SmramState
+    := Addr -> Value.
+
+  Definition update
+             (a: Addr)
+             (v: Value)
+             (s: SmramState)
+    : SmramState :=
+    fun (a': Addr)
+    => if addr_eq a a'
+       then v
+       else s a'.
+
+  Definition Smram_step
+             (A: Type)
+             (i: IMCH A)
+             (s: SmramState)
+    : SmramState :=
+    match i with
+    | WriteMem a v true (* Privileged Write Access within the SMRAM *)
+      => if Smramdec a
+         then update a v s
+         else s
+    | _ (* Any other primitives should not trigger a SMRAM change --*)
+      => s
+    end.
+
+  (** *** Requirements
+
+      Because we do not allow anyone to modify the [smram_lock]
+      register, we do not have any requirement the Memory Controller
+      user (typically the CPU) needs to enforce.
+
+   *)
+
+  Definition Smram_requirements
+             (A: Type)
+             (i: IMCH A)
+             (S: SmramState)
+    : Prop :=
+    True.
+
+  (** *** Promises
+
+      An Enforcer Component needs to stay syncronize with the abstract
+      state of the Smram, that is deliver the value written by a
+      privileged memory access.
+
+   *)
+
+  Definition Smram_promises
+             (A: Type)
+             (i: IMCH A)
+             (ret: A)
+             (s: SmramState)
+    : Prop :=
+    match i with
+    | ReadMem a true (* Privileged Reads match the shadow Smram *)
+      => ret ~= (s a)
+    | _ (* No Guarantee otherwise *)
+      => True
+    end.
+
+  (** *** Definition
+
+   *)
+
+  Definition smram_contract
+             (s: SmramState)
+    : Contract SmramState IMCH :=
+    {| abstract := s
+     ; abstract_step := Smram_step
+     ; requirements := Smram_requirements
+     ; promises := Smram_promises
+     |}.
+
 End SMRAM_EXAMPLE.
