7ca1606906b5d4bb0a7f5117e304aee99f1573b0
Thomas Letan <thomas.letan@ssi.gouv.fr>
2017-09-07 13:37:53 +0200

    Pave the road for an executable program written in Coq

--- extraction/extraction.v
+++ extraction/extraction.v
@@ -3,22 +3,22 @@ Require Import FreeSpec.Interp.
 Require Import FreeSpec.Program.
 Require Import FreeSpec.Oracle.Oracle.
 
+Require Import FreeSpec.Control.Option.
 Require Import FreeSpec.Examples.Map.
 Require Import FreeSpec.Specs.Memory.
-
-(** * Haskell Extraction Plugin
- *)
-Add LoadPath "./libs/haskell-extraction/ocaml/".
-
-Require Import HaskellExtraction.HaskellExtraction.
-
-Hextraction , Map.IMap.
-
-(** * Ocaml Regular Extraction
-
- *)
+Require Import FreeSpec.Specs.x86.MCH.MCH.
 
 Extraction Language Haskell.
+
+(* TODO: this does not work:
+> Set Extraction Conservative Types.
+
+   Coq output:
+> File "./extraction/extraction.v", line 31, characters 0-184:
+> Anomaly:
+> File "plugins/extraction/extraction.ml", line 824, characters 3-9: Assertion failed.
+> Please report at http://coq.inria.fr/bugs/.
+ *)
 
 Require Import ExtrHaskellBasic.
 Require Import ExtrHaskellNatInt.
@@ -26,7 +26,7 @@ Require Import ExtrHaskellNatInt.
 Extract Inlined Constant Datatypes.fst => "fst".
 Extract Inlined Constant Datatypes.snd => "snd".
 
-Extraction "FreeSpec"
-           Program.runProgram' Memory.mem
-           Control.Identity.Identity Control.Identity.identity_Monad
-           Memory.append Map.IMap test_interpreter.
+Cd "extraction/hs".
+Separate Extraction FreeSpec.MonadInterp FreeSpec.Specs.x86.MCH.MCH
+         FreeSpec.Control.Option.
+Cd "../..".
