6dd7ba0275acb5120a51518739ccf51009da209a
Thomas Letan <thomas.letan@ssi.gouv.fr>
2017-11-20 16:02:09 +0100

    Improve the Db modules organization

--- poc/Db.v
+++ poc/Db.v
@@ -13,16 +13,17 @@ Local Open Scope free_weq_scope.
 Generalizable All Variables.
 Set Implicit Arguments.
 
-Module Type DB.
+Module Type DbSpec.
   Parameters (K:         Type)
-             (WEqK:      WEq K)
-             (WEqBoolK:  WEqBool K)
              (Res:       Type)
              (Err:       Type).
+End DbSpec.
+
+Module DB (Spec:  DbSpec).
 
   Record Entity :=
-    { key:  K
-      ; val:  Res
+    { key:  Spec.K
+    ; val:  Spec.Res
     }.
 
   (** * Simple
@@ -33,13 +34,36 @@ Module Type DB.
     : Interface :=
   | select (selector:  Entity -> bool)
     : Query (list Entity)
-  | insert (value:     Res)
+  | insert (value:     Spec.Res)
     : Query Entity
   | update (selector:  Entity -> bool)
-           (update:    Res -> Res)
+           (update:    Spec.Res -> Spec.Res)
     : Query unit
   | delete (selector:  Entity -> bool)
     : Query unit.
+
+  Module DSL.
+    Definition select
+               (sel:  Entity -> bool)
+    : Program Query (list Entity) :=
+      instr (select sel).
+
+    Definition update
+               (sel:  Entity -> bool)
+               (up:   Spec.Res -> Spec.Res)
+    : Program Query unit :=
+      instr (update sel up).
+
+    Definition delete
+               (sel:  Entity -> bool)
+      : Program Query unit :=
+      instr (delete sel).
+
+    Definition insert
+               (v:  Spec.Res)
+      : Program Query Entity :=
+      instr (insert v).
+  End DSL.
 
   (** ** Functional Contract
 
@@ -48,7 +72,7 @@ Module Type DB.
   Module QuerySemantics.
     Definition State
     : Type :=
-      K -> option Res.
+      Spec.K -> option Spec.Res.
 
     Definition empty
       : State :=
@@ -56,11 +80,12 @@ Module Type DB.
       => None.
 
     Definition query_insert
-               (k:  K)
-               (r:  Res)
+              `{WEqBool Spec.K}
+               (k:  Spec.K)
+               (r:  Spec.Res)
                (s:  State)
       : State :=
-      fun (k':  K)
+      fun (k':  Spec.K)
       => if k ?= k'
          then Some r
          else s k'.
@@ -69,7 +94,7 @@ Module Type DB.
                (select:  Entity -> bool)
                (s:  State)
       : State :=
-      fun (k:  K)
+      fun (k:  Spec.K)
       => match s k with
          | Some x
            => if select {| key := k; val := x |}
@@ -81,10 +106,10 @@ Module Type DB.
 
     Definition query_update
                (select:  Entity -> bool)
-               (up:      Res -> Res)
+               (up:      Spec.Res -> Spec.Res)
                (s:  State)
       : State :=
-      fun (k:  K)
+      fun (k:  Spec.K)
       => match s k with
          | Some x
            => if select {| key := k; val := x |}
@@ -95,6 +120,7 @@ Module Type DB.
          end.
 
     Definition query_step
+              `{WEqBool Spec.K}
                (A:      Type)
                (q:      Query A)
                (x:      A)
@@ -119,8 +145,8 @@ Module Type DB.
       True.
 
     Definition is_key_of
-               (k:      K)
-               (v:      Res)
+               (k:      Spec.K)
+               (v:      Spec.Res)
                (state:  State)
       : Prop :=
       match state k with
@@ -129,8 +155,8 @@ Module Type DB.
       end.
 
     Inductive is_key_of_l
-              (k:  K)
-              (v:  Res)
+              (k:  Spec.K)
+              (v:  Spec.Res)
       : list Entity -> Prop :=
     | is_key (l:   list Entity)
       : is_key_of_l k v (cons {| key := k; val := v |} l)
@@ -140,7 +166,8 @@ Module Type DB.
       : is_key_of_l k v (cons entity l).
 
     Fixpoint key_count
-             (k:  K)
+            `{WEqBool Spec.K}
+             (k:  Spec.K)
              (l:  list Entity)
       : nat :=
       match l with
@@ -151,9 +178,10 @@ Module Type DB.
       end.
 
     Definition query_select_promises_res_wf
+              `{WEqBool Spec.K}
                (res:       list Entity)
       : Prop :=
-      forall (k:  K),
+      forall (k:  Spec.K),
         key_count k res < 2.
 
     Definition query_select_promises_state_to_res
@@ -161,8 +189,8 @@ Module Type DB.
                (res:       list Entity)
                (state:     State)
       : Prop :=
-      forall (k:  K)
-             (v:  Res),
+      forall (k:  Spec.K)
+             (v:  Spec.Res),
         is_key_of k v state
         -> selector {| key := k; val := v |} = true
         -> is_key_of_l k v res.
@@ -172,17 +200,18 @@ Module Type DB.
                (res:       list Entity)
                (state:     State)
       : Prop :=
-      forall (k:  K)
-             (v:  Res),
+      forall (k:  Spec.K)
+             (v:  Spec.Res),
         is_key_of_l k v res
         -> selector {| key := k; val := v |} = true
            /\ is_key_of k v state.
 
     Inductive query_promises
+             `{WEqBool Spec.K}
       : forall (A:  Type),
         Query A -> A -> State -> Prop :=
     | insert_promises (state:  State)
-                      (v:      Res)
+                      (v:      Spec.Res)
                       (res:    Entity)
                       (Hval:   val res = v)
                       (Hkey:   state (key res) = None)
@@ -196,13 +225,14 @@ Module Type DB.
       : query_promises (select selector) res state
     | update_promises (state:     State)
                       (selector:  Entity -> bool)
-                      (up:        Res -> Res)
+                      (up:        Spec.Res -> Spec.Res)
       : query_promises (update selector up) tt state
     | delete_promises (state:     State)
                       (selector:  Entity -> bool)
       : query_promises (delete selector) tt state.
 
     Definition query_contract
+              `{WEqBool Spec.K}
       : Contract State Query :=
       {| abstract_step := query_step
          ; requirements := query_requirements
@@ -210,6 +240,7 @@ Module Type DB.
       |}.
 
     Lemma query_contract_compliance
+         `{WEqBool Spec.K}
           {A:      Type}
           (p:      Program Query A)
           (state:  State)
@@ -222,7 +253,7 @@ Module Type DB.
       + constructor.
         ++ apply IHp.
         ++ intros int Hint.
-           apply H.
+           apply H1.
     Qed.
   End QuerySemantics.
 
@@ -241,6 +272,7 @@ Module Type DB.
 
   Module TransactionSemantics.
     Record State
+          `{WEqBool Spec.K}
     : Type :=
       { view:                 QuerySemantics.State
       ; interpreter:          Interp Query
@@ -248,9 +280,10 @@ Module Type DB.
       }.
 
     (* For the record, an implementation of this function using
-    Program (the Coq Keyword, not the FreeSpec Monad) would failed
+    Program (the Coq Spec.Keyword, not the FreeSpec Monad) would failed
     here because of universe error. *)
     Definition transaction_step
+           `{WEqBool Spec.K}
             (A:      Type)
             (instr:  i A)
             (x:      A)
@@ -273,6 +306,7 @@ Module Type DB.
     Qed.
 
     Definition transaction_req
+              `{WEqBool Spec.K}
                (A:      Type)
                (instr:  i A)
                (state:  State)
@@ -280,6 +314,7 @@ Module Type DB.
       True.
 
     Definition transaction_promises
+              `{WEqBool Spec.K}
                (A:      Type)
                (instr:  i A)
                (x:      A)
@@ -291,6 +326,7 @@ Module Type DB.
       end.
 
     Definition transaction_contract
+               `{WEqBool Spec.K}
       : Contract State i :=
       {| abstract_step := transaction_step
        ; requirements := transaction_req
