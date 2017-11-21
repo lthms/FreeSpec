ac7a5a67e209c1a040b1e95f2cb583f81efc83ed
Thomas Letan <thomas.letan@ssi.gouv.fr>
2017-11-21 16:03:52 +0100

    Prove the [concurrence_is_possible] theorem

--- theories/Concurrent.v
+++ theories/Concurrent.v
@@ -2,309 +2,297 @@ Require Import Coq.Program.Equality.
 
 Require Import FreeSpec.Abstract.
 Require Import FreeSpec.Contract.
+Require Import FreeSpec.Contract.Constant.
 Require Import FreeSpec.Control.
 Require Import FreeSpec.Interface.
 Require Import FreeSpec.Interp.
 Require Import FreeSpec.Program.
+Require Import FreeSpec.Refine.
+Require Import FreeSpec.WEq.
 
-(** * Regular Execution
+Local Open Scope free_weq_scope.
 
- *)
-
-Inductive Execution
-          {I:    Interface}
-          {A:    Type}
-          (int:  Interp I)
-  : Program I A -> A -> Interp I -> Prop :=
-| exec_ret (x:  A)
-  : Execution int (ret x) x int
-| exec_instr (i:  I A)
-  : Execution int (instr i) (evalInstruction int i) (execInstruction int i)
-| exec_bind {B:           Type}
-            (p:           Program I B)
-            (f:           B -> Program I A)
-            (x:           B)
-            (y:           A)
-            (int' int'':  Interp I)
-            (Hleft:       @Execution I B int p x int')
-            (Hright:      Execution int' (f x) y int'')
-  : Execution int (pbind p f) y int''.
-
-Theorem exec_prop_exec_comp
-        {I:    Interface}
-        {A:    Type}
-        (int:  Interp I)
-        (p:    Program I A)
-  : Execution int p (evalProgram int p) (execProgram int p).
-Proof.
-  revert int;
-    induction p;
-    intros int.
-  + constructor.
-  + constructor.
-  + rewrite eval_program_bind_assoc.
-    rewrite exec_program_bind_assoc.
-    apply exec_bind with (x:=evalProgram int p)
-                         (int':=execProgram int p).
-    ++ apply IHp.
-    ++ apply H.
-Qed.
-
-Theorem exec_prop_unique_res
-        {I:           Interface}
-        {A:           Type}
-        (int:         Interp I)
-        (p:           Program I A)
-        (x y:         A)
-        (int' int'':  Interp I)
-  : Execution int p x int'
-    -> Execution int p y int''
-    -> x = y /\ int' = int''.
-Proof.
-  revert int int' int''.
-  induction p; intros int int' int''.
-  + intros H1 H2.
-    inversion H1; subst.
-    inversion H2; subst.
-    split; reflexivity.
-  + intros H1 H2.
-    inversion H1; subst.
-    inversion H2; subst.
-    split; reflexivity.
-  + intros H1 H2.
-    inversion H1; simplify_eqs; simpl_existTs.
-    inversion H2; simplify_eqs; simpl_existTs.
-    subst.
-    assert (Hx:  x0 = x1 /\ int'0 = int'1). {
-      apply IHp with (int:=int); [ apply Hleft
-                                 | apply Hleft0
-                                 ].
-    }
-    destruct Hx as [Hx Hint].
-    subst.
-    apply H with (b:=x1)
-                 (int:=int'1); [ apply Hright
-                               | apply Hright0
-                               ].
-Qed.
-
-Inductive stream
-          {I:  Interface}
-          (P:  forall (A:  Type), I A -> Prop)
+Inductive sequence
+          (I:  Interface)
   : Type :=
-| stop
-  : stream P
-| pick {A:     Type}
-       (i:     I A)
-       (H:     P A i)
-       (rest:  stream P)
-  : stream P.
+| sequence_nil
+  : sequence I
+| sequence_cons {A:     Type}
+                (i:     I A)
+                (rest:  sequence I)
+  : sequence I.
 
-Arguments stop [I P].
-Arguments pick [I P A] (i H rest).
+Arguments sequence_nil [I].
+Arguments sequence_cons [I A] (i rest).
 
-Fixpoint stream_to_prog
+Inductive correct_sequence
+          {I:    Interface}
+          (req:  forall (A:  Type),
+              I A -> Prop)
+  : sequence I -> Prop :=
+| correct_sequence_cons {A:      Type}
+                        (i:      I A)
+                        (Hreq:   req A i)
+                        (rest:   sequence I)
+                        (H:      correct_sequence req rest)
+  : correct_sequence req (sequence_cons i rest)
+| correct_sequence_nil
+  : correct_sequence req sequence_nil.
+
+Notation "l '=¦' req" :=
+  (correct_sequence req l)
+    (at level 50, no associativity).
+
+Fixpoint sequence_to_prog
          {I:   Interface}
-         {P:   forall (A:  Type), I A -> Prop}
-         (st:  stream P)
+         (st:  sequence I)
   : Program I unit :=
   match st with
-  | stop
+  | sequence_nil
     => pure tt
-  | pick i _ rest
-    => instr i >>= fun _ => stream_to_prog rest
+  | sequence_cons i rest
+    => instr i >>= fun _ => sequence_to_prog rest
   end.
 
-Inductive ConcurrentExecution
-          {I:    Interface}
-          {A:    Type}
-          (int:  Interp I)
-          (P:    forall (A:  Type),
-              I A -> Prop)
-  : Program I A -> A -> Interp I -> Prop :=
-| conc_exec_ret (x:  A)
-  : ConcurrentExecution int P (ret x) x int
-| conc_exec_instr (st:    stream P)
-                  (int':  Interp I)
-                  (Hst:   Execution int (stream_to_prog st) tt int')
-                  (i:     I A)
-  : ConcurrentExecution int P (instr i) (evalInstruction int' i) (execInstruction int' i)
-| conc_exec_bind {B:           Type}
-                 (p:           Program I B)
-                 (f:           B -> Program I A)
-                 (x:           B)
-                 (y:           A)
-                 (int' int'':  Interp I)
-                 (Hleft:       @ConcurrentExecution I B int P p x int')
-                 (Hright:      ConcurrentExecution int' P (f x) y int'')
-  : ConcurrentExecution int P (pbind p f) y int''.
+CoInductive stream
+            (I:  Interface)
+  : Type :=
+| stream_cons (seq:       sequence I)
+              (infinite:  stream I)
+  : stream I.
 
-Theorem execution_is_concurrent_execution
-        {I:         Interface}
-        {A:         Type}
-        (int int':  Interp I)
-        (p:         Program I A)
-        (x:         A)
-  : forall (P:  forall (A:  Type),
-               I A -> Prop),
-    Execution int p x int'
-    -> ConcurrentExecution int P p x int'.
+Arguments stream_cons [I] (seq infinite).
+
+CoInductive correct_stream
+            {I:    Interface}
+            (req:  forall (A:  Type),
+                I A -> Prop)
+  : stream I -> Prop :=
+| restricted_stream_cons (seq:   sequence I)
+                         (Hseq:  correct_sequence req seq)
+                         (next:  stream I)
+                         (Hnext:  correct_stream req next)
+  : correct_stream req (stream_cons seq next).
+
+Notation "st '*=¦' req" :=
+  (correct_stream req st)
+    (at level 50, no associativity).
+
+Definition pick
+           {I:   Interface}
+           (st:  stream I)
+  : sequence I :=
+  match st with
+  | stream_cons seq _
+    => seq
+  end.
+
+Definition consume
+           {I:   Interface}
+           (st:  stream I)
+  : stream I :=
+  match st with
+  | stream_cons _ infinite
+    => infinite
+  end.
+
+Fact correct_stream_pick_correct_sequence
+     {I:    Interface}
+     (req:  forall (A:  Type), I A -> Prop)
+     (st:   stream I)
+     (H:    st *=¦ req)
+  : pick st =¦ req.
 Proof.
-  intros P.
-  revert int int' x; induction p; intros int int' x.
-  + intros H.
-    inversion H; subst.
-    constructor.
-  + intros H.
-    inversion H; subst.
-    apply conc_exec_instr with (st:=stop).
-    constructor.
-  + intros Hbind.
-    inversion Hbind; simplify_eqs; simpl_existTs.
-    subst.
-    apply IHp in Hleft.
-    apply H in Hright.
-    apply conc_exec_bind with (x1:=x0)
-                              (int'1:=int'0); [ apply Hleft
-                                              | apply Hright
-                                              ].
+  inversion H; subst.
+  exact Hseq.
 Qed.
 
-Notation "int '-[' p ']->' int'" :=
-  (Execution int (p >>= fun _ => pure tt) tt int')
-    (at level 50, no associativity).
+Fact correct_stream_consume_correct_stream
+     {I:    Interface}
+     (req:  forall (A:  Type), I A -> Prop)
+     (st:   stream I)
+     (H:    st *=¦ req)
+  : consume st *=¦ req.
+Proof.
+  inversion H; subst.
+  exact Hnext.
+Qed.
 
-Notation "int '=[' p '||' P ']=>' int'" :=
-  (ConcurrentExecution int P (p >>= fun _ => pure tt) tt int')
-    (at level 50, no associativity).
+CoFixpoint concurrent_interp
+         {I:    Interface}
+         (int:  Interp I)
+         (st:   stream I)
+  : Interp I :=
+  interp (fun {A:  Type}
+              (i:  I A)
+          => (evalInstruction (execProgram int (sequence_to_prog (pick st))) i,
+              concurrent_interp (execInstruction (execProgram int
+                                                              (sequence_to_prog (pick st))
+                                                 ) i)
+                                (consume st))).
 
-(** * Abstract Execution
-
- *)
-
-Inductive AbstractExecution
+Inductive non_interference_requirement
+          {W:    Type}
           {I:    Interface}
-          {A W:  Type}
           (c:    Contract W I)
-  : W -> Program I A -> A -> W -> Prop :=
-| abstract_exec_ret (w:  W)
-                    (x:  A)
-  : AbstractExecution c w (ret x) x w
-| abstract_exec_instr (w:      W)
-                      (i:      I A)
-                      (Hreq:   requirements c i w)
-                      (x:      A)
-                      (Hprom:  promises c i x w)
-  : AbstractExecution c w (instr i) x (abstract_step c i x w)
-| abstract_exec_bind {B:         Type}
-                     (w w' w'':  W)
-                     (p:         Program I B)
-                     (x:         B)
-                     (f:         B -> Program I A)
-                     (y:         A)
-                     (Hright:    AbstractExecution (A:=B) c w p x w')
-                     (Hleft:     AbstractExecution c w' (f x) y w'')
-  : AbstractExecution c w (pbind p f) y w''.
+          (req:  forall (A:  Type), I A -> Prop)
+  : Prop :=
+| concurrent_contracts_req (Hreq: forall {A:  Type}
+                                         (i:  I A)
+                                         (w:  W),
+                               req A i -> requirements c i w)
+                           (Hni: forall {A:  Type}
+                                        (i:  I A)
+                                        (x:  A)
+                                        (w:  W),
+                               req A i -> abstract_step c i x w = w)
+  : non_interference_requirement c req.
 
-Lemma compliant_interpreter_correct_program_abstract_execution
+Notation "c \\ req" :=
+  (non_interference_requirement c req)
+    (at level 50, no associativity, req at next level).
+
+Lemma correct_sequence_rewrite_abstract_state
       {I:     Interface}
-      {A W:   Type}
-      (c:     Contract W I)
+      {W:     Type}
       (w:     W)
+      (c:     Contract W I)
+      (req:   forall (A:  Type),
+          I A -> Prop)
+      (seq:   sequence I)
+      (Hseq:  seq =¦ req)
       (int:   Interp I)
-      (p:     Program I A)
       (Hint:  int |= c[w])
-      (Hp:    p =| c[w])
-  : AbstractExecution c w p (evalProgram int p) (contract_derive p int c w).
+      (Hni:   c \\ req)
+  : contract_derive (sequence_to_prog seq) int c w = w.
 Proof.
-  revert Hp Hint.
-  revert w int.
-  induction p; intros w int Hp Hint.
-  + constructor.
-  + constructor; [| apply Hint ];
-      inversion Hp;
-      simplify_eqs;
-      simpl_existTs;
-      subst;
-      exact Hreq.
-  + rewrite <- contract_derive_pbind.
-    rewrite eval_program_bind_assoc.
-    apply abstract_exec_bind with (w' :=  contract_derive p int c w)
-                                  (x  :=  evalProgram int p).
-    ++ apply IHp; [| apply Hint ].
-       apply correct_pbind_correct_left_operand with (f0:=f).
-       apply Hp.
-    ++ rewrite abstract_exec_exec_program_same.
-       apply H.
-       apply correct_pbind_correct_right_operand; [ exact Hint
-                                                  | exact Hp
-                                                  ].
-       erewrite <- abstract_exec_exec_program_same.
-       apply enforcer_compliant_enforcer.
-       exact Hint.
-       apply correct_pbind_correct_left_operand with (f0 :=  f).
-       exact Hp.
+  revert Hint; revert int w.
+  induction seq; intros int w Hint.
+  + reflexivity.
+  + cbn.
+    unfold program_bind.
+    rewrite <- contract_derive_pbind.
+    inversion Hseq; simplify_eqs; simpl_existTs; subst.
+    rewrite IHseq.
+    ++ apply Hni.
+       apply Hreq.
+    ++ apply H0.
+    ++ apply enforcer_compliant_enforcer; [ exact Hint |].
+       constructor.
+       destruct Hni.
+       apply Hreq0.
+       exact Hreq.
 Qed.
 
-Inductive ConcurrentAbstractExecution
-          {I:    Interface}
-          {A W:  Type}
-          (c:    Contract W I)
-          (P:    forall (A:  Type),
-              I A -> Prop)
-  : W -> Program I A -> A -> W -> Prop :=
-| concur_abstract_exec_ret (w:  W)
-                           (x:  A)
-  : ConcurrentAbstractExecution c P w (ret x) x w
-| concur_abstract_exec_instr (w w':     W)
-                             (st:       stream P)
-                             (Hbefore:  AbstractExecution c w (stream_to_prog st) tt w')
-                             (i:        I A)
-                             (Hreq:     requirements c i w')
-                             (x:        A)
-                             (Hprom:    promises c i x w')
-  : ConcurrentAbstractExecution c P w' (instr i) x (abstract_step c i x w')
-| concur_abstract_exec_bind {B:         Type}
-                            (w w' w'':  W)
-                            (p:         Program I B)
-                            (x:         B)
-                            (f:         B -> Program I A)
-                            (y:         A)
-                            (Hright:    ConcurrentAbstractExecution (A:=B) c P w p x w')
-                            (Hleft:     ConcurrentAbstractExecution c P w' (f x) y w'')
-  : ConcurrentAbstractExecution c P w (pbind p f) y w''.
-
-Theorem abstract_execution_is_concurrent_abstract_execution
-        {I:    Interface}
-        {A W:  Type}
-        (c:    Contract W I)
-        (w:    W)
-        (P:    forall (A:  Type), I A -> Prop)
-        (p:    Program I A)
-        (x:    A)
-        (w':   W)
-  : AbstractExecution c w p x w'
-    -> ConcurrentAbstractExecution c P w p x w'.
+Lemma correct_sequence_correct_program
+      {I:     Interface}
+      {W:     Type}
+      (w:     W)
+      (c:     Contract W I)
+      (req:   forall (A:  Type),
+          I A -> Prop)
+      (seq:   sequence I)
+      (Hseq:  seq =¦ req)
+      (Hni:   c \\ req)
+  : sequence_to_prog seq =| c[w].
 Proof.
-  revert w w'.
-  induction p; intros w w'.
-  + intros H; inversion H; subst.
+  revert w.
+  induction seq; intros w.
+  + constructor.
+  + cbn.
+    unfold program_bind.
     constructor.
-  + intros H; inversion H; subst.
-    eapply concur_abstract_exec_instr with (w0 :=  w)
-                                           (st :=  stop).
-    ++ constructor.
-    ++ exact Hreq.
-    ++ exact Hprom.
-  + intros Hbind.
-    inversion Hbind;
-      simplify_eqs;
-      simpl_existTs;
-      subst.
-    apply concur_abstract_exec_bind with (w'1 :=  w'0)
-                                         (x1  :=  x0).
-    ++ apply IHp.
-       exact Hright.
-    ++ apply H.
-       apply Hleft.
+    ++ inversion Hseq; simplify_eqs; simpl_existTs; subst.
+       constructor.
+       destruct Hni.
+       apply Hreq0.
+       exact Hreq.
+    ++ intros int Hint.
+       inversion Hseq; simplify_eqs; simpl_existTs; subst.
+       assert (Heq:  contract_derive (instr i) int c w = w). {
+         cbn.
+         destruct Hni.
+         apply Hni.
+         apply Hreq.
+       }
+       rewrite Heq.
+       apply IHseq.
+       exact H0.
+Qed.
+
+Theorem concurrence_is_possible
+        {I:     Interface}
+        {W:     Type}
+        (w:     W)
+        (c:     Contract W I)
+        (req:   forall (A:  Type),
+            I A -> Prop)
+        (st:    stream I)
+        (Hst:   st *=¦ req)
+        (int:   Interp I)
+        (Hint:  int |= c[w])
+        (Hni:   c \\ req)
+  : concurrent_interp int st |= c[w].
+Proof.
+  revert Hint.
+  revert Hst.
+  revert int st w.
+  cofix; intros int st w Hst Hint.
+  constructor.
+  + cbn.
+    assert (pick st =¦ req)
+      by (apply correct_stream_pick_correct_sequence; assumption).
+    assert (consume st *=¦ req)
+      by (apply correct_stream_consume_correct_stream; assumption).
+    inversion Hst; subst.
+    cbn.
+    intros A i Hreq.
+    erewrite <- correct_sequence_rewrite_abstract_state; [ idtac
+                                                         | exact Hseq
+                                                         | exact Hint
+                                                         | exact Hni
+                                                         ].
+    erewrite <- correct_sequence_rewrite_abstract_state in Hreq; [ idtac
+                                                         | exact Hseq
+                                                         | exact Hint
+                                                         | exact Hni
+                                                         ].
+    assert (Hint': execProgram int (sequence_to_prog seq) |= c[contract_derive (sequence_to_prog seq) int c w]). {
+      erewrite <- abstract_exec_exec_program_same.
+      apply enforcer_compliant_enforcer.
+      exact Hint.
+      apply correct_sequence_correct_program with (req0 :=  req).
+      + exact Hseq.
+      + exact Hni.
+    }
+    apply Hint'.
+    apply Hreq.
+  + cbn.
+    assert (pick st =¦ req)
+      by (apply correct_stream_pick_correct_sequence; assumption).
+    assert (consume st *=¦ req)
+      by (apply correct_stream_consume_correct_stream; assumption).
+    inversion Hst; subst.
+    cbn.
+    intros A i Hreq.
+    remember (execProgram int (sequence_to_prog seq)) as int'.
+    remember (evalInstruction int' i) as x.
+    assert (Hp:  sequence_to_prog seq =| c[w]). {
+      apply correct_sequence_correct_program with (req0 :=  req).
+      exact Hseq.
+      exact Hni.
+    }
+    assert (Hint':  int' |= c[w]). {
+      rewrite <- (correct_sequence_rewrite_abstract_state w c req seq Hseq int Hint Hni).
+      rewrite Heqint'.
+      erewrite <- abstract_exec_exec_program_same.
+      apply enforcer_compliant_enforcer.
+      + exact Hint.
+      + exact Hp.
+    }
+    apply concurrence_is_possible.
+    apply Hnext.
+    rewrite Heqx.
+    apply Hint'.
+    apply Hreq.
 Qed.
