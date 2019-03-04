cdd7932bae18188746abfd80dff879ffed0481ab
Yann Regis-Gianas <yrg@irif.fr>
2019-03-04 18:33:04 +0100

    Fix contributor list.

--- exec/src/interfaces.ml
+++ exec/src/interfaces.ml
@@ -2,7 +2,7 @@
  * Copyright (C) 2018–2019 ANSSI
  *
  * Contributors:
- * 2019 Thomas Letan <thomas.letan@ssi.gouv.fr>
+ * 2019 Yann Régis-Gianas <yrg@irif.fr>
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
