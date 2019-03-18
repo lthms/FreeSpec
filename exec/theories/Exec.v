3a9e0e5b53e2a6f677ad9ff4ad7a688c93e0132a
Thomas Letan <thomas.letan@ssi.gouv.fr>
2019-03-18 13:07:54 +0100

    exec: Introduce [withComponent] to provide pure Gallina handlers

--- exec/theories/Exec.v
+++ exec/theories/Exec.v
@@ -18,7 +18,60 @@
  * along with this program. If not, see <http://www.gnu.org/licenses/>.
  *)
 
-Require Export FreeSpec.Exec.Debug.
+Require Import Prelude.Control.
+
+Require Import FreeSpec.Component.
 Require Import FreeSpec.Compose.
+Require Import FreeSpec.Program.
+
+Require Export FreeSpec.Exec.Debug.
+
+Local Open Scope free_scope.
+Local Open Scope prelude_scope.
 
 Declare ML Module "exec_plugin".
+
+#[local]
+Fixpoint extends
+         {Ix J:  Type -> Type}
+         {A S:   Type}
+         (s:     S)
+         (comp:  Component J S Ix)
+         (p:     Program (J <+> Ix) A)
+: Program Ix (A * S) :=
+  match p with
+  | Pure x
+    => Pure (x, s)
+  | Request (InL e) f
+    => res <- comp _ e s;
+       extends (snd res) comp (f (fst res))
+  | Request (InR e) f
+    => Request e (fun x => extends s comp (f x))
+  end.
+
+(** With FreeSpec.Exec, it becomes possible to interpret a term of type [Program
+    Ix A], where [Ix] depends on the plugins loaded by the user.
+
+    In addition, we provide [withComponent], a helper function to extend the set
+    of interfaces that can be executed with FreeSpec [Component]s. There are
+    several advantages to rely on [withComponent] rather than writing an OCaml
+    plugin, the most important being a handler in Coq is not part of the
+    TCB. Besides, it can be verified as any FreeSpec [Component]. *)
+Definition withComponent
+         {Ix J:         Type -> Type}
+         {A S:          Type}
+         (** A [Component] carries its own state. The [initializer] is a
+             computation to construct the initial state of the component *)
+         (initializer:  Program Ix S)
+         (** The [Component] used to implement a semantics for J *)
+         (comp:         Component J S Ix)
+         (** The [finalizer] is a clean-up computation to “destruct” the
+             [Component] final state *)
+         (finalizer:    S -> Program Ix unit)
+         (** A computation to interpret, that uses [J] in addition to [Ix]. *)
+         (p:            Program (J <+> Ix) A)
+  : Program Ix A :=
+  s <- initializer;
+  res <- extends s comp p;
+  finalizer (snd res);;
+  pure (fst res).
