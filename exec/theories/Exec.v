607b6d7087d32d0b8312b14b994b19e1fa3848cf
Thomas Letan <thomas.letan@ssi.gouv.fr>
2019-03-18 13:07:54 +0100

    exec: Introduce [withComponent] to provide pure Gallina handlers

--- exec/theories/Exec.v
+++ exec/theories/Exec.v
@@ -18,7 +18,46 @@
  * along with this program. If not, see <http://www.gnu.org/licenses/>.
  *)
 
-Require Export FreeSpec.Exec.Debug.
+Require Import Prelude.Control.
+
+Require Import FreeSpec.Component.
 Require Import FreeSpec.Compose.
+Require Import FreeSpec.Program.
+
+Require Export FreeSpec.Exec.Debug.
+
+Local Open Scope free_scope.
+Local Open Scope prelude_scope.
+
+#[local]
+Fixpoint extends
+         {Ix J:  Type -> Type}
+         {A S:   Type}
+         (s:     S)
+         (comp:  Component J S Ix)
+         (p:     Program (J <+> Ix) A)
+: Program Ix (A * S) :=
+  match p with
+  | Pure x
+    => Pure (x, s)
+  | Request (InL e) f
+    => res <- comp _ e s;
+       extends (snd res) comp (f (fst res))
+  | Request (InR e) f
+    => Request e (fun x => extends s comp (f x))
+  end.
+
+Definition withComponent
+         {Ix J:         Type -> Type}
+         {A S:          Type}
+         (initializer:  Program Ix S)
+         (comp:         Component J S Ix)
+         (finalizer:    S -> Program Ix unit)
+         (p:            Program (J <+> Ix) A)
+  : Program Ix A :=
+  s <- initializer;
+  res <- extends s comp p;
+  finalizer (snd res);;
+  pure (fst res).
 
 Declare ML Module "exec_plugin".
