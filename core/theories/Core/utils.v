0115da146c8b8fca2519dfeee35b5eceedac088b
Thomas Letan <thomas.letan@ssi.gouv.fr>
2019-07-20 13:03:04 +0200

    refactor: Rewrite Interface, Program and Semantics from Core
    In this patch, we introduce a complete rewriting of FreeSpec.Core key
    modules, as a first step towards a complete clean-up of the
    framework. Compare to the initial libraries, this rewriting provides
    several benefits:
    
    - We provide a hint database named freespec
    - We correctly scope them inside the FreeSpec.Core namespace
    - We do not rely on the functional extensionality axiom of the Coq
      standard library, meaning FreeSpec.Core revert to be axiom-free
    - We use a less opinionated coding style, taking our inspiration from
      libraries such as MetaCoq

---
+++ core/theories/Core/utils.v
@@ -0,0 +1,37 @@
+(* FreeSpec
+ * Copyright (C) 2018–2019 ANSSI
+ *
+ * Contributors:
+ * 2018–2019 Thomas Letan <thomas.letan@ssi.gouv.fr>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ *)
+
+Class CCons (x rst : Type) : Type :=
+  { proj_x : x
+  ; proj_rst : rst
+  }.
+
+Class CNil : Type.
+
+Ltac find_class_list_instance :=
+  match goal with
+  | |- CCons _ _
+    => constructor; [typeclasses eauto | find_class_list_instance]
+  | |- CNil
+    => constructor
+  end.
+
+Hint Extern 20 (CCons _ _) => find_class_list_instance : typeclass_instances.
+Hint Extern 20 (CNil _ _) => find_class_list_instance : typeclass_instances.
