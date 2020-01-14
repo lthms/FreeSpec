6f5dfe50bb6532ddace04b37cbec10a18291c459
Thomas Letan <thomas.letan@ssi.gouv.fr>
2020-01-14 17:31:06 +0100

    refactor: Make Exec's conversion functions for lists tailrec

--- exec/src/coqlist.ml
+++ exec/src/coqlist.ml
@@ -7,22 +7,24 @@ let coqlist_nil typ = mkApp (Ind.List.mkConstructor "nil", of_list [typ])
 
 let coqlist_cons typ trm rstrm = mkApp (Ind.List.mkConstructor "cons", of_list [typ; trm; rstrm])
 
-(* TODO: tailrec version *)
-let rec list_of_coqlist of_coq trm =
-  let (c, args) = app_full trm in
-  match (Ind.List.constructor_of c, args) with
-  | (Some Nil_list, [_t]) -> []
-  | (Some Cons_list, [_t; coqx; coqrst]) ->
-     let x = of_coq coqx in
-     let rst = list_of_coqlist of_coq coqrst in
-     x :: rst
-  | _ -> raise (UnsupportedTerm "of_coqlist: not a Coq list")
+let list_of_coqlist of_coq =
+  let rec aux cont trm =
+    let (c, args) = app_full trm in
+    match (Ind.List.constructor_of c, args) with
+    | (Some Nil_list, [_t]) -> cont []
+    | (Some Cons_list, [_t; coqx; coqrst]) ->
+       let x = of_coq coqx in
+       aux (fun next -> cont (x :: next)) coqrst
+    | _ -> raise (UnsupportedTerm "of_coqlist: not a Coq list")
+  in aux (fun x -> x)
 
 (* TODO: tailrec version *)
-let rec list_to_coqlist typ to_coq = function
-  | [] -> coqlist_nil typ
-  | x :: rst ->
-     coqlist_cons typ (to_coq x) (list_to_coqlist typ to_coq rst)
+let list_to_coqlist typ to_coq =
+  let rec aux cont = function
+    | [] -> cont (coqlist_nil typ)
+    | x :: rst ->
+       aux (fun next -> cont (coqlist_cons typ (to_coq x) next)) rst
+  in aux (fun x -> x)
 
 let coqlist_t typ =
   mkApp (Ind.List.mkInductive, of_list [typ])
